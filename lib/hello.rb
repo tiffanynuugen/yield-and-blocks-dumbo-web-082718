b788929cc48d5aad3ee8b9b98881d0b408564d8c
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-08-16 19:56:13 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -5,7 +5,10 @@ def hello_t(array)
      i = i + 1
    end
    array
-end
+ else
+     puts "Hey! No block was given!"
+   end
+ end
 
 hello_t(["Tim", "Tom", "Jim"]) do |name|
  if name.start_with?("T")
